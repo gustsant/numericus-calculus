6e827ab7d584d664f2976ad77575b5b1e7aa4ece
Gustavo <92876438+gustsant@users.noreply.github.com>
2023-06-29 13:48:51 -0300

    Update Q4_1.m

--- Question 4/Q4_1.m
+++ Question 4/Q4_1.m
@@ -22,36 +22,46 @@ hist_p3 = zeros([30 2]);
 hist_temp = zeros([30 1]);
 
 while cont < lim
-  # Calculando o próximo valor das velocidades
+  #Calculando o próximo valor das posições
+  p1_n = p1 + h * v1;
+  p2_n = p2 + h * v2;
+  p3_n = p3 + h * v3;
+
+  #Calculando o próximo valor das velocidades
   dv1 = der_v1(p1, p2, p3, m2, m3);
-  v1 = v1 + h * dv1;
+  v1_n = v1 + h * dv1;
 
   dv2 = der_v2(p1, p2, p3, m1, m3);
-  v2 = v2 + h * dv2;
+  v2_n = v2 + h * dv2;
 
   dv3 = der_v3(p1, p2, p3, m2, m1);
-  v3 = v3 + h * dv3;
+  v3_n = v3 + h * dv3;
 
-  # Calculando o próximo valor das posições
-  p1 = p1 + h * v1;
-  p2 = p2 + h * v2;
-  p3 = p3 + h * v3;
+  #Atualizando valores
+  p1 = p1_n;
+  p2 = p2_n;
+  p3 = p3_n;
 
-  # Guardando esses valores para o gráfico
-  hist_p1(cont + 1, 1) = p1(1); # x
-  hist_p1(cont + 1, 2) = p1(2); # y
+  v1 = v1_n;
+  v2 = v2_n;
+  v3 = v3_n;
 
-  hist_p2(cont + 1, 1) = p2(1);
-  hist_p2(cont + 1, 2) = p2(2);
+  #Guardando esses valores para o gráfico
+  hist_p1(cont + 1,1) = p1(1); # x
+  hist_p1(cont + 1,2) = p1(2); # y
 
-  hist_p3(cont + 1, 1) = p3(1);
-  hist_p3(cont + 1, 2) = p3(2);
+  hist_p2(cont + 1,1) = p2(1);
+  hist_p2(cont + 1,2) = p2(2);
+
+  hist_p3(cont + 1,1) = p3(1);
+  hist_p3(cont + 1,2) = p3(2);
 
   hist_temp(cont + 1) = cont;
 
-  # Avançando na iteração
+  #Avançando na iteração
   cont = cont + 1;
 end
+
 
 % Exibição dos resultados
 disp('p1 = [');
@@ -68,5 +78,3 @@ disp(']');
 
 # Plotando os gráficos
 plot(hist_p1(:,1),hist_p1(:,2),hist_p2(:,1),hist_p2(:,2),hist_p3(:,1),hist_p3(:,2));
-
-
